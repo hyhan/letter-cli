4eb7ddc48b332e7fa8f1f43c93baac0dea407248
hui <125173679@qq.com>
2022-05-09 15:49:10 +0800

    feat: get npm info

--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -1,7 +1,65 @@
 'use strict'
 
-module.exports = getNpmInfo
+const semver = require('semver')
+const axios = require('axios')
+const urlJoin = require('url-join')
 
-function getNpmInfo() {
-  // TODO
+async function getNpmInfo(npmName, registry = 'https://registry.npmjs.org') {
+  if (!npmName) {
+    return null
+  }
+  const npmPath = urlJoin(registry, npmName)
+  return axios
+    .get(npmPath)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.data
+      }
+      return null
+    })
+    .catch((e) => {
+      return Promise.reject(e)
+    })
+}
+
+async function getNpmVersions(npmName, registry) {
+  const data = await getNpmInfo(npmName, registry)
+  if (data) {
+    return Object.keys(data)
+  } else {
+    return []
+  }
+}
+
+async function getSemverVersions(baseVersion, versions) {
+  return versions.filter((version) =>
+    semver
+      .satisfies(version, `>${baseVersion}`)
+      .sort((a, b) => (semver.gt(b, a) ? 1 : -1))
+  )
+}
+
+async function getNpmSemverVersion(baseVersion, npmName, registry) {
+  const versions = await getNpmVersions(npmName, registry)
+  const semverVersions = getSemverVersions(baseVersion, versions)
+  if (semverVersions && semverVersions.length > 0) {
+    return semverVersions[0]
+  }
+  return null
+}
+
+async function getNpmLatestVersion(npmName, registry) {
+  const versions = await getNpmVersions(npmName, registry)
+  if (versions) {
+    return versions.sort((a, b) => semver.gt(b, a)[0])
+  }
+  return null
+}
+
+module.exports = {
+  getNpmInfo,
+  getNpmVersions,
+  getNpmLatestVersion,
+  getSemverVersions,
+  getNpmSemverVersion,
 }
